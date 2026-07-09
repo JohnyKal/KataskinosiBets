cfae6a6ccb35bb15dd669cf9cf4fa52cb202e9c4
John Kallergis <kallergisgiannis09@gmail.com>
2026-07-09 22:55:39 +0300

    bluh

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import { connectDB } from "./db.js";
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Connect to MongoDB
 connectDB();
