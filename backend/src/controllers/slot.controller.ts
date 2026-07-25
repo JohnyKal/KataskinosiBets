7e1bfc395e0faa57be1f02adbb1072a9b7d3f00e
John Kallergis <kallergisgiannis09@gmail.com>
2026-07-25 14:49:42 +0300

    less winning chances

--- backend/src/controllers/slot.controller.ts
+++ backend/src/controllers/slot.controller.ts
@@ -2,11 +2,11 @@ import type { Request, Response } from "express";
 import { UserModel } from "../models/User.model.js";
 
 const symbols = [
-  { text: "ΠΑΙΔΙ", emoji: "✨", reward: 12, chance: 17 },
-  { text: "ΣΟΣ", emoji: "🔥", reward: 16, chance: 2 },
-  { text: "ΟΜΑΔΑΡΧΗΣ", emoji: "🎉", reward: 18, chance: 0.7 },
-  { text: "ΚΟΙΝΟΤΑΡΧΗΣ", emoji: "⭐", reward: 21, chance: 0.25 },
-  { text: "ΑΡΧΗΓΟΣ", emoji: "🍀", reward: 50, chance: 0.05 },
+  { text: "ΠΑΙΔΙ", emoji: "✨", reward: 12, chance: 21 },
+  { text: "ΣΟΣ", emoji: "🔥", reward: 16, chance: 2.8 },
+  { text: "ΟΜΑΔΑΡΧΗΣ", emoji: "🎉", reward: 18, chance: 0.8 },
+  { text: "ΚΟΙΝΟΤΑΡΧΗΣ", emoji: "⭐", reward: 21, chance: 0.3 },
+  { text: "ΑΡΧΗΓΟΣ", emoji: "🍀", reward: 50, chance: 0.1 },
 ];
 
 const randomSymbol = () =>
